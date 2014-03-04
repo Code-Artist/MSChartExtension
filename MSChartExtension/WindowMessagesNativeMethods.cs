5f5806a753c13c2d322ec4f370c53973d615e293
Code Artist <codearteng@gmail.com>
2014-03-04 23:02:24 +0800

    Fixed PInvoke Warning
    Fixed Code Analysis Warning CA1901: P/Invoke declarations should be
    portable. Reported by wiredancer

--- MSChartExtension/WindowMessagesNativeMethods.cs
+++ MSChartExtension/WindowMessagesNativeMethods.cs
@@ -10,11 +10,11 @@ namespace System.Windows.Forms.DataVisualization.Charting
     {
         #region [ Suspend / Resume Drawing ]
         [DllImport("user32.dll")]
-        private static extern long SendMessage(IntPtr hWnd, Int32 wMsg, [MarshalAs(UnmanagedType.Bool)] bool wParam, Int32 lParam);
+        private static extern IntPtr SendMessage(IntPtr hWnd, Int32 wMsg, IntPtr wParam, IntPtr lParam);
 
         private const int WM_SETREDRAW = 11;
-        public static void SuspendDrawing(Control parent) { SendMessage(parent.Handle, WM_SETREDRAW, false, 0); }
-        public static void ResumeDrawing(Control parent) { SendMessage(parent.Handle, WM_SETREDRAW, true, 0); parent.Refresh(); }
+        public static void SuspendDrawing(Control parent) { SendMessage(parent.Handle, WM_SETREDRAW, IntPtr.Zero, IntPtr.Zero); }
+        public static void ResumeDrawing(Control parent) { SendMessage(parent.Handle, WM_SETREDRAW, new IntPtr(1), IntPtr.Zero); parent.Refresh(); }
         #endregion
 
     }
